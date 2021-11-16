ac972687176e34d5846fe8e64d33d98a378339fb
bless-botzz <91579936+bless-botzz@users.noreply.github.com>
2021-11-17 05:20:11 +0700

    Update aov.js

--- shop/aov.js
+++ shop/aov.js
@@ -8,17 +8,11 @@ FORMAT	ID
 VOUCHER AOV
 		
 40VC	  Rp 10,000.00		
-90VC	  Rp 20,000.00		
-130VC   Rp 30,000.00		
-180VC   Rp 40,000.00		
-230VC   Rp 50,000.00		
-470VC   Rp 100,000.00		
+90VC	  Rp 19,800.00				
+230VC   Rp 49,000.00		
+470VC   Rp 98,500.00		
 950VC	Rp 195,000.00		
-1.430VC 	Rp 290,000.00		
-2.390VC	 Rp 485,000.00		
-4.800VC	 Rp 970,000.00		
-24.050VC   Rp 4.845,000.00		
-48.200VC   Rp 9.685,000.00		
+1.430VC  Rp 293,000.00			
 
 Untuk pay / pembayan silahkan ketik ${prefix}pay
 `
